57386c017aed13d164067c31a1f5cbc304b238e4
Aime <aimeiradukunda113@gmail.com>
2025-11-20 12:27:24 +0200

    making templates that customer can view the product

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register_view, login_view, logout_view,home_view,dashboard,vendor_dashboard
+from .views import register_view, login_view, logout_view,home_view,dashboard,vendor_dashboard,product_list,product_detail
 
 urlpatterns = [
      path('', home_view, name='home'),  # <-- home page
@@ -9,4 +9,6 @@ urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
      path('dashboard/vendor/',vendor_dashboard, name='vendor_dashboard'),
      path('home/', home_view, name='home'),
+       path('products/', product_list, name='product_list'),
+       path('products/<int:id>/', product_detail, name='product_detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RegisterForm
 from django.contrib.auth.decorators import login_required
+from products.models import product
+from django.shortcuts import render, get_object_or_404
+
 
 def register_view(request):
     if request.method == "POST":
@@ -29,7 +32,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect("home")  # change to your homepage
+            return redirect("dashboard")  # change to your homepage
         else:
             messages.error(request, "Invalid username or password")
 
@@ -48,3 +51,11 @@ def dashboard(request):
 @login_required
 def vendor_dashboard(request):
     return render(request, 'accounts/vendor_dashboard.html')
+@login_required
+def product_list(request):
+    products = product.objects.all()
+    return render(request, 'accounts/product_list.html', {'products': products})
+@login_required
+def product_detail(request, id):
+    prod = get_object_or_404(product, id=id)
+    return render(request, 'accounts/product_detail.html', {'product': prod})
